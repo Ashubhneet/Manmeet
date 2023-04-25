2417dc17f6390b37c479446bec7d90f525493995
manmeetkau <manmeet.kaur1@tothenew.com>
2023-04-25 23:03:04 +0530

    Validation in API,Activation Link

--- src/main/java/com/demo/project/securityConfig/SecurityConfiguration.java
+++ src/main/java/com/demo/project/securityConfig/SecurityConfiguration.java
@@ -1,31 +1,76 @@
 package com.demo.project.securityConfig;
-
+/*
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+//@EnableMethodSecurity
 public class SecurityConfiguration {
+    private JwtAuthEntryPoint jwtAuthEntryPoint;
+    //    private JWTAuthenticationFilter jwtAuthenticationFilter;
+    @Autowired
+    private CustomUserDetailService customUserDetailsService;
+
+    public SecurityConfiguration(CustomUserDetailService customUserDetailsService
+            ,JwtAuthEntryPoint jwtAuthEntryPoint) {
+        this.customUserDetailsService = customUserDetailsService;
+        this.jwtAuthEntryPoint = jwtAuthEntryPoint;
+    }
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)throws Exception {
-        httpSecurity.csrf()
-                .disable()
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthEntryPoint)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .authenticationProvider(authenticationProvider())
+//                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .and()
                 .authorizeHttpRequests()
-                .anyRequest()
-                .permitAll()
+                .requestMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .httpBasic();
+        httpSecurity.addFilterBefore(jwtAuthenticationFilter() , UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
+
+//    @Bean
+//    public AuthenticationProvider authenticationProvider(){
+//        DaoAuthenticationProvider authenticationProvider=new DaoAuthenticationProvider();
+//        authenticationProvider.setUserDetailsService(userDetailsService());
+//        authenticationProvider.setPasswordEncoder(passwordEncoder());
+//        return authenticationProvider;
+//    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
-}
+    @Bean
+    public JWTAuthenticationFilter jwtAuthenticationFilter(){
+        return new JWTAuthenticationFilter();
+    }
+}*/
